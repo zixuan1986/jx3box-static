a276a41817ed07e9be9dfc367022cdc0842b8072
iRuxu <rx6@qq.com>
2020-03-16 07:00:59 +0800

    fix translate macro

--- src/js/single/single-macro.js
+++ src/js/single/single-macro.js
@@ -8,7 +8,6 @@
 import JX3_MACRO from '../widget/macro.js'
 import JX3_QIXUE from '../widget/qixue.js'
 import JX3_XINFA from '../widget/xinfa.js'
-const {cn2tw} = require('../widget/translator');
 
 var $ = jQuery;
 jQuery(function($){
@@ -71,18 +70,30 @@ function bind_copyEvent(){
 }
 
 //繁简切换 TODO:以后根据整站语言预设模式自动翻译原文
+const axios = require('axios');
+import translator from'../widget/translator';
+console.log(111)
 function bind_translateEvent(){
     let $translator = $('#w-translator-macro')
-    $translator.one('click',function (){
-        $('.m-macro-box .m-macro-code-format').each(function (){
-            $(this).find('.jx3macro-condition,.jx3macro-skill-name').each(function (i,val){
-                $(this).text(cn2tw($(this).text()))
-            })
+    new Promise((resolve,reject)=>{
+        axios.get(`https://cdn.jsdelivr.net/npm/@jx3box/jx3box-dict/dict.json?v=${Date.now()}`).then((res) => {
+            resolve(res.data)
+        }).catch(function (err){
+            console.error('[jx3box/app/translator] axios exception')
+            reject(err)
         })
-        $translator.addClass('on')
-        setTimeout(function (){
-            $translator.removeClass('on')
-            $translator.addClass('resolved')
-        },800)
+    }).then((dict) => {
+        $translator.one('click',function (){
+            $('.m-macro-box .m-macro-code-format').each(function (){
+                $(this).find('.jx3macro-condition,.jx3macro-skill-name').each(function (i,val){
+                    $(this).text(translator($(this).text(),dict))
+                })
+            })
+            $translator.addClass('on')
+            setTimeout(function (){
+                $translator.removeClass('on')
+                $translator.addClass('resolved')
+            },800)
+        })
     })
 }
